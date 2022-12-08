e9a820e50414915866e17dec10f1513ca26210b4
nesati <59739799+nesati@users.noreply.github.com>
2022-12-08 16:15:32 +0100

    Add database interface

--- utils/interface.py
+++ utils/interface.py
@@ -4,9 +4,65 @@ class Module:
 
 
 class Database(Module):
+    """
+    The database provides a way to store additional information about images (mainly tags).
+
+    Only one database can be used at one time.
+    """
     def __init__(self, bus, loop):
+        """
+        :param bus: EventBus instance
+        :param loop: AsyncIO loop
+        """
         self.bus = bus
         self.loop = loop
+
+        self.bus.add_listener('new_image', lambda i: self.add_image(i[0]))
+
+    async def add_image(self, uid):
+        """
+        Creates a record for a given image in the database.
+        :param uid: The image's unique ID
+        """
+        raise NotImplementedError()
+
+    async def check_image(self, uid):
+        """
+        Checks if the there exists a record for a given image in the database.
+        :param uid: The image's unique ID
+        :return: bool
+        """
+        raise NotImplementedError()
+
+    async def get_tags(self, uid):
+        """
+        Lists tags of a given image.
+        :param uid: The image's unique ID
+        """
+        raise NotImplementedError()
+
+    async def add_tag(self, uid, tag):
+        """
+        Adds a tag to a given image.
+        :param uid: The image's unique ID
+        :param tag: Tag name
+        """
+        raise NotImplementedError()
+
+    async def remove_tag(self, uid, tag):
+        """
+        Removes a tag from a given image.
+        :param uid: The image's unique ID
+        :param tag: Tag name
+        """
+        raise NotImplementedError()
+
+    async def list_tags(self):
+        """
+        Lists all tags in the database.
+        :return: list of tag names
+        """
+        raise NotImplementedError()
 
 
 class Backend(Module):
