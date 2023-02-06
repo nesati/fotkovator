796bdccb86d78f21987c94a05e8f272ec9346b72
nesati <59739799+nesati@users.noreply.github.com>
2023-02-07 00:17:19 +0100

    Implement some exif data extraction

--- modules/backend/localfs/main.py
+++ modules/backend/localfs/main.py
@@ -1,9 +1,12 @@
 import asyncio
 import os
+from datetime import datetime
 from io import BytesIO
 
 import aiofiles.os
 import PIL
+import plum
+from exif import Image as Exif, DATETIME_STR_FORMAT
 from PIL import Image
 
 from utils.interface import Backend
@@ -17,13 +20,41 @@ class LocalfsBackend(Backend):
 
     async def get_image(self, path):
         async with aiofiles.open(path, mode='rb') as f:
-            return Image.open(BytesIO(await f.read())), path, None, {
+            img_file = BytesIO(await f.read())
+            metadata = {
                 "uri": path,
                 "path": os.path.abspath(path),
                 "file_created": await aiofiles.os.path.getctime(path),
                 "file_modified": await aiofiles.os.path.getmtime(path),
                 "file_size": await aiofiles.os.path.getsize(path),
             }
+            img = Exif(img_file)
+            if img.has_exif:
+                metadata['exif'] = img.get_all()  # save all exif tags for use in modules
+
+                # extract common tags
+                camera = []
+                camera.append(img.get('make', None))
+                camera.append(img.get('model', None))
+                camera = list(filter(lambda x: x is not None, camera))
+                if camera:
+                    metadata['camera'] = ' '.join(camera)
+
+                if dt_str := img.get('datetime_original', None):
+                    metadata['datetime_original'] = datetime.strptime(dt_str, DATETIME_STR_FORMAT)
+
+                if dt_str := img.get('datetime_digitized', None):
+                    metadata['datetime_digitized'] = datetime.strptime(dt_str, DATETIME_STR_FORMAT)
+
+                # TODO gps
+
+                # TODO exif thumbnail
+
+            img_file.seek(0)
+            img = Image.open(img_file)
+            metadata['width'] = img.width
+            metadata['height'] = img.height
+            return img, path, None, metadata
 
     async def rescan(self, *args):
         async def check_file(path):
@@ -32,7 +63,7 @@ class LocalfsBackend(Backend):
                     image = await self.get_image(path)
                     await self.bus.emit('new_image', image)
                     await self.bus.emit('done', image[1])
-                except PIL.UnidentifiedImageError:
+                except (PIL.UnidentifiedImageError, plum.exceptions.UnpackError):
                     pass
 
         async def worker(q):
